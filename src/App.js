5f2fe71063049879c8494af3d10ba4468f1c1f6c
Yukesh PS <108916377+Yukesh420@users.noreply.github.com>
2023-03-29 10:49:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import World from './Team/Team';
+function App() {
+  return (
+    <div className="App">
+     <World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Team/Team.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div>This is a Team component</div>
+  )
+}
+
+export default World
